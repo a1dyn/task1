5fdbdd47defb856a0f4a16dfd0d92c8d26c0ae8a
aidynzhumakadyr <aidynzhumaadyr@gmail.com>
2023-06-05 16:51:35 +0600

    apply

--- employees/urls.py
+++ employees/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
-    path('', LoginAPIView.as_view(), name = 'login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout'),
     path("employees/<int:pk>/", EmployeeDetailView.as_view(), name = 'employee_detail'),
     path("employees/", EmployeeView.as_view(), name= 'employee_list'),
     path("leave-requests/<int:pk>/", LeaveRequestEditView.as_view(), name = 'leave_requests_update'),
